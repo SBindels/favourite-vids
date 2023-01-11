61dc459754c541ddf3196b848c0d2dd89450bff6
Sjoerd Bindels <sjoerdbindels@MacBook-Pro.local>
2023-01-11 12:16:36 +0100

    added styling to page and new clickVideo function

--- app.js
+++ app.js
@@ -13,12 +13,23 @@ const displayVideos = () => {
         <li onclick="clickVideo(event, '${id}')">
             <img class="thumbnail" src="https://i3.ytimg.com/vi/${id}/sddefault.jpg" alt="Cover image
             for YouTube video with id ${id}">
-            <button class="delete-btn" > $times;</button>  
+            <button class="delete-btn" > &times;</button>  
         </li>
     `).join("");
     videosContainer.innerHTML = videosHTMLString;
 
 };
+
+const clickVideo = (event, id) => {
+    console.log(event, id);
+    if (event.target.classList.contains('delete-btn')) {
+        youTubeVideoIds = youTubeVideoIds.filter(i => i != id);
+        localStorage.setItem('youTubeVideoIds', JSON.stringify(youTubeVideoIds));
+        displayVideos();
+    }else {
+
+    }
+}
 
 const saveVideo = (e) => {
     e.preventDefault();
@@ -27,7 +38,7 @@ const saveVideo = (e) => {
     videoIdInput.value = "";
     localStorage.setItem('youTubeVideoIds', JSON.stringify(youTubeVideoIds));
     displayVideos();
-}
+};
 
 loadVideos();
 displayVideos();
