df7ae5dce53f45d193a20f965a6cba641cf47d3f
Sjoerd Bindels <sjoerdbindels@MacBook-Pro.local>
2023-01-19 09:56:51 +0100

    fixed css/js loading issue + correcting folders

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-console.log("Hello world");
-
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+//require('dotenv').config();
+
+const port = 3000;
+
+app.set('view engine', 'ejs');
+//app.set('view', './view');
+
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("app")
+    res.render("index")
 })
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+})
